b0a886454770fe75262aa9aac7cc18091a863f9e
kyungsoon.kang <kyungsoon.kang@tmapmobility.com>
2023-12-07 12:19:38 +0900

    헤더 파싱 테스트

--- tests/shp-reader.test.js
+++ tests/shp-reader.test.js
@@ -1,3 +1,18 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import ShpReader from '../modules/shp-reader.js';
+
 const rootPath = process.cwd();
 
-console.log(rootPath);
+const arrayBuffer = readFile('resources/Duraklar.shp');
+const reader = new ShpReader(arrayBuffer);
+
+for (const feature of reader.featureIterator()) {
+    console.log(feature);
+}
+
+function readFile(relativePath) {
+    const shpPath = path.resolve(rootPath, relativePath);
+    const result = fs.readFileSync(shpPath);
+    return result.buffer;
+}
